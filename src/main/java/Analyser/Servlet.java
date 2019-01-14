65c845305671ae2a4294d2ee89dc33871c96d2d9
AIhpJ <36286344+AIhpJ@users.noreply.github.com>
2019-01-14 19:48:59 +0800

    Update Servlet.java

--- src/main/java/Analyser/Servlet.java
+++ src/main/java/Analyser/Servlet.java
@@ -1,41 +1,76 @@
 package Analyser;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.Enumeration;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 @WebServlet(name = "Servlet", urlPatterns = {"postdata"}, loadOnStartup = 1)
 public class Servlet extends HttpServlet
 {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/MoniterData?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "MoniterData";
-    private static final String PASSWORD = "MoniterData";
-    private Connection conn;
+    Connection conn;
+    String driver = "com.mysql.cj.jdbc.Driver";
+    String url = "jdbc:mysql://localhost:3306/fuwuwaibao?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8&useSSL=false";
+    String user = "root";
+    String password = "JHP691009jhp";
+    Statement stmt = null;
 
     public void init()
     {
-        try
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn= DriverManager.getConnection(DB_URL,USER,PASSWORD);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Database connecting failed");
-            e.printStackTrace();
-        }
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException
     {
-        response.getWriter().print(request.getParameter("baseURI"));
+    	String baseURI = request.getParameter("baseURI");
+    	String date = request.getParameter("date");
+    	String device = request.getParameter("device");
+    	String element = request.getParameter("element");
+        response.getWriter().print(baseURI+"\n");
+        response.getWriter().print(date+"\n");
+        response.getWriter().print(device+"\n");
+        response.getWriter().print(element+"\n");
+        Connection conn;
+        Statement stmt = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        
+            conn = DriverManager.getConnection(url,user,password);
+        
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM fangwenjilu";
+            ResultSet rs = stmt.executeQuery(sql);
+            
+    		
+            while(rs.next()){
+                int page  = rs.getInt("page");
+    
+                System.out.print(page);
+                System.out.print("\n");
+            }
+            String sql1 = "INSERT INTO `fangwenjilu` (`date`, `IPaddress`, `device`, `SESSION`, `page`, `element`) VALUES ('"+date+"', '"+baseURI+"', '"+device+"', '1', '1', '"+element+"')";
+			stmt.executeUpdate(sql1);
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+        }
+        
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
